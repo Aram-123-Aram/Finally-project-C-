71d736ab6afdbdbe96e844228f2d23714802c33a
Aram <armsir-8182@mail.ru>
2022-12-02 23:51:41 +0300

    Finding summa of numbers from M to N using Recursia

---
+++ Exercise 66_itog_proj/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+Например:
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30    */
+
+Console.Write("Enter the first number M= ");
+int.TryParse(Console.ReadLine()!, out int M);
+Console.Write("Enter the first number N= ");
+int.TryParse(Console.ReadLine()!, out int N);
+
+int SumMN(int m, int n)
+{
+    if (m == n) return m;
+    else return m + SumMN(m+1, n);
+}
+Console.Write($"-> {SumMN(M, N)}");
