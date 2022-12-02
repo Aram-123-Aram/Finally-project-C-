2f27f75ef634b2d46da69a3e500c21861f9c32de
Aram <armsir-8182@mail.ru>
2022-12-02 23:23:15 +0300

    Showing the numbers from n to 1 using Recursia

---
+++ Exercise 64_itog_proj/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии. 
+Например:
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1   */
+
+Console.Write("Enter the number N= ");
+int.TryParse(Console.ReadLine()!, out int N);
+
+void ShowNumbers(int n)
+{
+    if(n == 1) {Console.Write($"{n}"); }
+    else {Console.Write($"{n}, "); 
+          ShowNumbers(n-1); }
+}
+Console.Write($"                     -> ");
+ShowNumbers(N);
